369fb38b0b81ef29b9631f66392ccf1b222eb2c9
Luis Villalobos <luisevm24@outlook.com>
2020-05-04 01:19:45 -0500

    Initial commit

---
+++ src/test/java/com/luisenricke/cuestionarioolvidadizo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisenricke.cuestionarioolvidadizo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
