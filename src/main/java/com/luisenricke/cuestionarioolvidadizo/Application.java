93810770d5108ff78d799a6ae9b51b0e8daa33a4
Luis Villalobos <luisevm24@outlook.com>
2020-05-08 22:37:18 -0500

    Add simple logback configurations

--- src/main/java/com/luisenricke/cuestionarioolvidadizo/Application.java
+++ src/main/java/com/luisenricke/cuestionarioolvidadizo/Application.java
@@ -1,13 +1,20 @@
 package com.luisenricke.cuestionarioolvidadizo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        LOG.info("This is a simple LOG with numbers {}, {}, {}", 1, 2, 3);
+    }
+
 
 }
